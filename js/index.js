39247842c582ec877b2b3371c7beb9e3e8fad2d2
clay-most <46730958+clay-most@users.noreply.github.com>
2019-05-03 17:16:38 -0600

    document event

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
+document.addEventListener('click', () => {alert("Ouch!!")});
+
 let header = document.querySelector(".main-navigation")
 header.addEventListener(`mouseover`, () => {header.style.backgroundColor = "pink"});
-
 // element.addEventListener(, (event) => {});
 // element.addEventListener(, (event) => {});
 // element.addEventListener(, (event) => {});
