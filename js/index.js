bafd912eaf716b491a20b8409813e6e99639f3c3
clay-most <46730958+clay-most@users.noreply.github.com>
2019-05-03 17:03:39 -0600

    Header event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+let header = document.querySelector(".main-navigation")
+header.addEventListener(`mouseover`, () => {header.style.backgroundColor = "pink"});
+
+// element.addEventListener(, (event) => {});
+// element.addEventListener(, (event) => {});
+// element.addEventListener(, (event) => {});
+// element.addEventListener(, (event) => {});
+// element.addEventListener(, (event) => {});
+// element.addEventListener(, (event) => {});
+// element.addEventListener(, (event) => {});
+// element.addEventListener(, (event) => {});
+// element.addEventListener(, (event) => {});
