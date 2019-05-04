e82a4e2a5fd651b68746558352d80fa1cbe4f3bc
clay-most <46730958+clay-most@users.noreply.github.com>
2019-05-03 19:40:36 -0600

    build ten events functionality issues

--- js/index.js
+++ js/index.js
@@ -1,13 +1,39 @@
-document.addEventListener('click', () => {alert("Ouch!!")});
+let bus = document.querySelector("header img");
+bus.addEventListener("click", () => {
+  bus.style.display = "none";
+});
 
-let header = document.querySelector(".main-navigation")
-header.addEventListener(`mouseover`, () => {header.style.backgroundColor = "pink"});
-// element.addEventListener(, (event) => {});
-// element.addEventListener(, (event) => {});
-// element.addEventListener(, (event) => {});
-// element.addEventListener(, (event) => {});
-// element.addEventListener(, (event) => {});
-// element.addEventListener(, (event) => {});
-// element.addEventListener(, (event) => {});
-// element.addEventListener(, (event) => {});
-// element.addEventListener(, (event) => {});
+let header = document.querySelector("header");
+header.addEventListener("dblclick", () => {
+  header.style.backgroundColor = "pink";
+});
+
+document.addEventListener("keydown", () => {
+  alert("Harder...");
+});
+
+document.addEventListener("offline", () => {
+    alert("Check your connection and come back later.");
+  });
+  
+
+let h1 = document.querySelector("h1");
+h1.addEventListener("mouseleave", () => {
+  h1.style.color = "red";
+});
+let nav = document.querySelectorAll("nav a");
+nav[0].addEventListener("mouseover", () => {
+  nav[0].style.color = "orange";
+});
+nav[1].addEventListener("mouseenter", () => {
+  nav[1].style.color = "yellow";
+});
+nav[2].addEventListener("mouseleave", () => {
+  nav[2].style.color = "green";
+});
+nav[3].addEventListener("mousemove", () => {
+  nav[3].style.color = "indigo";
+});
+
+let btn = document.querySelectorAll(".btn")
+btn[0].addEventListener("ontouchstart",()=>{btn[0].innerHTML = "Hello!"})
