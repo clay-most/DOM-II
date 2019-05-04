17957e05632acd7bc3e3fa1d3ace15bffbd1a8c4
clay-most <46730958+clay-most@users.noreply.github.com>
2019-05-04 10:50:54 -0600

    changed events/added stoppropagation

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 let bus = document.querySelector("header img");
 bus.addEventListener("click", () => {
   bus.style.display = "none";
+  event.stopPropagation();
+});
+
+document.addEventListener("click", () => {
+  alert("Ouch!");
 });
 
 let header = document.querySelector("header");
@@ -8,32 +13,36 @@ header.addEventListener("dblclick", () => {
   header.style.backgroundColor = "pink";
 });
 
-document.addEventListener("keydown", () => {
-  alert("Harder...");
+document.addEventListener("scroll", () => {
+  alert("Wee!!");
 });
 
 document.addEventListener("offline", () => {
-    alert("Check your connection and come back later.");
-  });
-  
+  alert("Check your connection and come back later.");
+});
 
 let h1 = document.querySelector("h1");
 h1.addEventListener("mouseleave", () => {
   h1.style.color = "red";
 });
+
 let nav = document.querySelectorAll("nav a");
 nav[0].addEventListener("mouseover", () => {
   nav[0].style.color = "orange";
 });
+
 nav[1].addEventListener("mouseenter", () => {
   nav[1].style.color = "yellow";
 });
 nav[2].addEventListener("mouseleave", () => {
   nav[2].style.color = "green";
 });
+
 nav[3].addEventListener("mousemove", () => {
   nav[3].style.color = "indigo";
 });
 
-let btn = document.querySelectorAll(".btn")
-btn[0].addEventListener("ontouchstart",()=>{btn[0].innerHTML = "Hello!"})
+let btn = document.querySelectorAll(".btn");
+btn[0].addEventListener("ontouchstart", () => {
+  btn[0].innerHTML = "Hello!";
+});
