b37efb45a40b90494d5dfd92d5dc932fc2b99b4d
clay-most <46730958+clay-most@users.noreply.github.com>
2019-10-03 17:18:37 -0600

    Add comments explaining code

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
+//Header image disappers when focused on//
 let bus = document.querySelector('header img');
-bus.addEventListener('click', () => {
+bus.addEventListener('focus', () => {
   bus.style.display = 'none';
-  event.stopPropagation();
+  
 });
 
+//When any image is clicked the window alerts a message//
 const ouch = document.querySelectorAll('img');
 ouch.forEach(index =>
   index.addEventListener('click', () => {
@@ -11,40 +13,50 @@ ouch.forEach(index =>
   })
 );
 
+//When the header is double clicked it turns pink//
 let header = document.querySelector('header');
 header.addEventListener('dblclick', () => {
   header.style.backgroundColor = 'pink';
 });
 
+//When anykey is pressed the window alerts a message//
 document.addEventListener('keypress', () => {
   alert('Youch!');
 });
 
+//If you try to use the page but do not have internet the window alerts a message//
 document.addEventListener('offline', () => {
   alert('Check your connection and come back later.');
 });
 
+//The h1 turns red when a mouse leaves it//
 let h1 = document.querySelector('h1');
 h1.addEventListener('mouseleave', () => {
   h1.style.color = 'red';
 });
 
+//Grabbing all the nav a tags// 
 let nav = document.querySelectorAll('nav a');
+//When moused over the fist a tag turns orange//
 nav[0].addEventListener('mouseover', () => {
   nav[0].style.color = 'orange';
 });
-
+//When a mouse enters the second a tag turns yellow//
 nav[1].addEventListener('mouseenter', () => {
   nav[1].style.color = 'yellow';
 });
-nav[2].addEventListener('mouseleave', () => {
+//When clicked the third a tag should turn green without triggering the pink header event listener//
+nav[2].addEventListener('click', () => {
   nav[2].style.color = 'green';
+  event.stopPropagation();
 });
 
+//When a mouse moves on it at all the fourth a tag turns indigo//
 nav[3].addEventListener('mousemove', () => {
   nav[3].style.color = 'indigo';
 });
 
+//When any button is touched the window allets a message//
 let btn = document.querySelectorAll('.btn');
 btn[0].addEventListener('ontouchstart', () => {
   btn[0].innerHTML = 'Hello!';
